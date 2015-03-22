2c417c7160fadd323d67f136421b652a6d9ef847
uriel avalos <uri@frankandrobot.com>
2015-03-22 12:25:44 -0400

    [refac] some mo' cleanup

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var Tags = require('./routes/tag-routes');
+var Routes = require('./lib/routes/tag-routes');
 var config = require('./config.json');
 
 
@@ -19,7 +19,7 @@ var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use('/tags', new Tags(db).router);
+app.use('/tags', new Routes(db).router);
 
 
 // catch 404 and forward to error handler
