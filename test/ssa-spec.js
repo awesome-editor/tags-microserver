20f27dd4b4868712abdc4aea373acda65bc44a47
Uriel Avalos <uriel.avalos@drillinginfo.com>
2015-03-22 23:20:39 -0500

    [ssa][refac]

--- test/ssa-spec.js
+++ test/ssa-spec.js
@@ -25,12 +25,10 @@ describe("ssa", function() {
         it("should work", function () {
 
             var k = 3,
-                target = {name: 0},
-                neighbors = $.range(11).map(function (x) {
-                    return {name: x}
-                }),
+                target = 0,
+                neighbors = $.range(11),
 
-                actual = $.pluck(ssa._findKNearestNeighbors(k, sim, target, neighbors), 'name');
+                actual = ssa.findKNearestNeighbors(k, sim, target, neighbors);
 
             expect(actual).toEqual([10, 9, 8]);
 
@@ -56,10 +54,10 @@ describe("ssa", function() {
          */
         it("should work", function() {
 
-            var target = {name: 0},
+            var target = 0,
                 threshold = 0,
-                nearestneighbors = [{name: 9}],
-                neighbors = [{name:8},{name:9},{name:10}],
+                nearestneighbors = [9],
+                neighbors = [8,9,10],
                 SSA = function(x,y) {
 
                     var matrix = {
@@ -68,20 +66,119 @@ describe("ssa", function() {
                             "9->10": 1
                         },
 
-                        index = x.name +"->"+ y.name;
+                        index = x +"->"+ y;
 
-                    if (x.name== y.name) return 1;
+                    if (x == y) return 1;
 
                     return matrix[index] ? matrix[index] : 0;
                 },
 
                 actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
                 actualSSA = $.pluck(actual, 'ssa'),
-                actualVals = $.pluck(actual, 'name');
+                actualVals = $.pluck(actual, 'value');
 
 
             expect(actualSSA).toEqual([1,1,0]);
             expect(actualVals).toEqual([8,9,10]);
+        });
+
+
+        /**
+         * Given
+         * target = 0,
+         * threshold = 0,
+         * nearest neighbors = [9]
+         * sim = euclidian distance
+         * neighbors = [7,8,9,10]
+         * and SSA that describes this hierarchy:
+         *   (8)->(9)->(10)
+         *   (7)->(10)
+         *
+         * Then
+         * SSAp(target,7) = SSA(7,9) = 0
+         * SSAp(target,8) = SSA(8,9) = 1 / 9 * 9 = 1;
+         * SSAp(target,9) = SSA(9,9) = 1
+         * SSAp(target,10) = SSA(10,9) = 0
+         */
+        it("should work 2", function() {
+
+            var target = 0,
+                threshold = 0,
+                nearestneighbors = [9],
+                neighbors = [7,8,9,10],
+                SSA = function(x,y) {
+
+                    var matrix = {
+                            "8->9": 1,
+                            "8->10": 1/2,
+                            "9->10": 1,
+                            "7->10": 1
+                        },
+
+                        index = x +"->"+ y;
+
+                    if (x == y) return 1;
+
+                    return matrix[index] ? matrix[index] : 0;
+                },
+
+                actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
+                actualSSA = $.pluck(actual, 'ssa'),
+                actualVals = $.pluck(actual, 'value');
+
+
+            expect(actualSSA).toEqual([0,1,1,0]);
+            expect(actualVals).toEqual([7,8,9,10]);
+        });
+
+
+        /**
+         * Given
+         * target = 0,
+         * threshold = 0,
+         * nearest neighbors = [7,9]
+         * sim = euclidian distance
+         * neighbors = [7,8,9,10]
+         * and SSA that describes this hierarchy:
+         *   (8)->(9)->(10)
+         *   (7)->(10)
+         *
+         * Then
+         * denom = 7+9 = 16
+         * SSAp(target,7) = [SSA(7,7)*sim + SSA(7,9)*sim] / 16  = [1*7 + 0] / 16 = 7/16
+         * SSAp(target,8) = [SSA(8,7)*7 + SSA(8,9)*9] / 16 = [0 + 9] / 16 = 9/16
+         * SSAp(target,9) = [SSA(9,7]*7 + SSA(9,9)*9] / 16 = [0 + 9] / 16 = 9/16
+         * SSAp(target,10) = [SSA(10,7)*7 + SSA(10,9)*9] / 16 = 0
+         */
+        it("should work 3", function() {
+
+            var target = 0,
+                threshold = 0,
+                nearestneighbors = [7,9],
+                neighbors = [7,8,9,10],
+                SSA = function(x,y) {
+
+                    var matrix = {
+                            "8->9": 1,
+                            "8->10": 1/2,
+                            "9->10": 1,
+                            "7->10": 1
+                        },
+
+                        index = x +"->"+ y;
+
+                    if (x== y) return 1;
+
+                    return matrix[index] ? matrix[index] : 0;
+                },
+
+                actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
+                actualSSA = $.pluck(actual, 'ssa'),
+                actualVals = $.pluck(actual, 'value');
+
+
+            expect(actualSSA).toEqual([7/16,9/16,9/16,0]);
+            expect(actualVals).toEqual([7,8,9,10]);
         });
     });
 });
