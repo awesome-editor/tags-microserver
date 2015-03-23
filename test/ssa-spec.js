5d2e996a62036a82a7bb5add052ee6a5a98138f5
Uriel Avalos <uriel.avalos@drillinginfo.com>
2015-03-22 23:35:22 -0500

    [ssa][refac]

--- test/ssa-spec.js
+++ test/ssa-spec.js
@@ -24,11 +24,14 @@ describe("ssa", function() {
          */
         it("should work", function () {
 
-            var k = 3,
-                target = 0,
-                neighbors = $.range(11),
+            var args = {
+                    k: 3,
+                    sim: sim,
+                    target: 0,
+                    neighbors: $.range(11)
+                },
 
-                actual = ssa.findKNearestNeighbors(k, sim, target, neighbors);
+                actual = ssa.findKNearestNeighbors(args);
 
             expect(actual).toEqual([10, 9, 8]);
 
@@ -54,26 +57,29 @@ describe("ssa", function() {
          */
         it("should work", function() {
 
-            var target = 0,
-                threshold = 0,
-                nearestneighbors = [9],
-                neighbors = [8,9,10],
-                SSA = function(x,y) {
+            var args = {
+                    target: 0,
+                    threshold: 0,
+                    nearestneighbors: [9],
+                    allneighbors: [8,9,10],
+                    sim: sim,
+                    SSA: function(x,y) {
 
-                    var matrix = {
-                            "8->9": 1,
-                            "8->10": 1/2,
-                            "9->10": 1
-                        },
+                        var matrix = {
+                                "8->9": 1,
+                                "8->10": 1 / 2,
+                                "9->10": 1
+                            },
 
-                        index = x +"->"+ y;
+                            index = x + "->" + y;
 
-                    if (x == y) return 1;
+                        if (x == y) return 1;
 
-                    return matrix[index] ? matrix[index] : 0;
+                        return matrix[index] ? matrix[index] : 0;
+                    }
                 },
 
-                actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
+                actual = ssa.SSAp(args),
                 actualSSA = $.pluck(actual, 'ssa'),
                 actualVals = $.pluck(actual, 'value');
 
@@ -102,27 +108,30 @@ describe("ssa", function() {
          */
         it("should work 2", function() {
 
-            var target = 0,
-                threshold = 0,
-                nearestneighbors = [9],
-                neighbors = [7,8,9,10],
-                SSA = function(x,y) {
-
-                    var matrix = {
-                            "8->9": 1,
-                            "8->10": 1/2,
-                            "9->10": 1,
-                            "7->10": 1
-                        },
-
-                        index = x +"->"+ y;
-
-                    if (x == y) return 1;
-
-                    return matrix[index] ? matrix[index] : 0;
+            var args = {
+                    target: 0,
+                    threshold: 0,
+                    nearestneighbors: [9],
+                    allneighbors: [7, 8, 9, 10],
+                    sim: sim,
+                    SSA: function (x, y) {
+    
+                        var matrix = {
+                                "8->9": 1,
+                                "8->10": 1 / 2,
+                                "9->10": 1,
+                                "7->10": 1
+                            },
+    
+                            index = x + "->" + y;
+    
+                        if (x == y) return 1;
+    
+                        return matrix[index] ? matrix[index] : 0;
+                    }
                 },
 
-                actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
+                actual = ssa.SSAp(args),
                 actualSSA = $.pluck(actual, 'ssa'),
                 actualVals = $.pluck(actual, 'value');
 
@@ -152,27 +161,30 @@ describe("ssa", function() {
          */
         it("should work 3", function() {
 
-            var target = 0,
-                threshold = 0,
-                nearestneighbors = [7,9],
-                neighbors = [7,8,9,10],
-                SSA = function(x,y) {
+            var args = {
+                    target: 0,
+                    threshold: 0,
+                    nearestneighbors: [7, 9],
+                    allneighbors: [7, 8, 9, 10],
+                    sim: sim,
+                    SSA: function (x, y) {
 
-                    var matrix = {
-                            "8->9": 1,
-                            "8->10": 1/2,
-                            "9->10": 1,
-                            "7->10": 1
-                        },
+                        var matrix = {
+                                "8->9": 1,
+                                "8->10": 1 / 2,
+                                "9->10": 1,
+                                "7->10": 1
+                            },
 
-                        index = x +"->"+ y;
+                            index = x + "->" + y;
 
-                    if (x== y) return 1;
+                        if (x == y) return 1;
 
-                    return matrix[index] ? matrix[index] : 0;
+                        return matrix[index] ? matrix[index] : 0;
+                    }
                 },
 
-                actual = ssa.SSAp(target, threshold, nearestneighbors, sim, SSA, neighbors),
+                actual = ssa.SSAp(args),
                 actualSSA = $.pluck(actual, 'ssa'),
                 actualVals = $.pluck(actual, 'value');
 
